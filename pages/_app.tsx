6199266661f399b645386db08a632fcc74a7a082
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2023-01-27 18:53:00 +0530

    Updated app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import '../component/services.css'
 import Head from 'next/head';
 import '../component/animationtext'
+import '../component/form'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
